fb1086e79a33f5cb3087ced8f0d5d6ab560fcc72
Franklin Aurelio <franklin.carvalho@unifesp.br>
2023-06-27 12:29:37 -0300

    feat: inicio do desenvolvimento e estruturação do layout

--- travel-app-swift-ui/travel-app-swift-ui/ContentView.swift
+++ travel-app-swift-ui/travel-app-swift-ui/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("SwiftUI ")
+       
+            GeometryReader{ view in
+                VStack{
+                    VStack{
+                        Text("Travels App")
+                            .foregroundColor(Color.white)
+                            .font(.custom("Avenir Black", size: 20))
+                            .padding(.top, 55)
+                        Text("SPECIAL")
+                            .foregroundColor(Color.white)
+                            .font(.custom("Avenir Book", size: 20))
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 30)
+                        Text("BRAZIL")
+                            .foregroundColor(Color.white)
+                            .font(.custom("Avenir Black", size: 23))
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 30)
+                        
+                        
+                    }
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: view.size.width , height: 180, alignment: .top)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.797, saturation: 0.492, brightness: 0.835)/*@END_MENU_TOKEN@*/)
+                    
+                    List{
+                        Text("Rio de Janeiro")
+                        Text("Ceará")
+                        Text("Atibaia")
+                        Text("Natal")
+                    }
+            }
         }
-        .padding()
     }
 }
 
