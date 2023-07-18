fbbb3c07da057aeb5cbfd423f31e28a71105ffc8
Franklin Aurelio <franklin.carvalho@unifesp.br>
2023-07-18 11:35:02 -0300

    feat: size class

--- travel-app-swift-ui/travel-app-swift-ui/views/CellTravelView.swift
+++ travel-app-swift-ui/travel-app-swift-ui/views/CellTravelView.swift
@@ -9,18 +9,25 @@ import SwiftUI
 
 struct CellTravelView: View {
     
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
     var viagem: Viagem
     
     var body: some View {
         VStack(alignment: .leading){
             Text(viagem.titulo)
+                .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14 : 24))
             Image(viagem.imagem)
                 .resizable()
-                .frame(height: 125)
+                .aspectRatio(contentMode: .fill)
+                .frame(height: self.horizontalSizeClass == .compact ? 125 : 200)
+                .clipped()
             HStack{
                 Text(viagem.quantidadeDeDias)
+                    .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14 : 24))
                 Spacer()
                 Text(viagem.valor)
+                    .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14 : 24))
             }
         }
     }
